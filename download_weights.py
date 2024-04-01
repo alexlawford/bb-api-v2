f6535493665a8b5fdf6c238be6575f12a1be4d8e
Alexander Browne <alexlawford@icloud.com>
2024-04-01 10:18:18 +0800

    changed to download file ULRs

--- download_weights.py
+++ download_weights.py
@@ -30,9 +30,9 @@ def download_file(url, file_name, directory):
 
 # Download and save files
 files = [
-    ('https://s3.us-west-1.wasabisys.com/bb-weights/queratograySketch_v10.safetensors?AWSAccessKeyId=000R8RBVN2HP618VW0U4&Expires=1710558861&Signature=xYiwZVL%2FAT7rInb5%2F9Iq4qi2D6E%3D', 'queratograySketch_v10.safetensors'),
-    ('https://s3.us-west-1.wasabisys.com/bb-weights/RealESRGAN_x2.pth?AWSAccessKeyId=000R8RBVN2HP618VW0U4&Expires=1710558893&Signature=PDVDoWN%2B2dSxsrjvJPpHmZP4gnM%3D', 'RealESRGAN_x2.pth'),
-    ('https://s3.us-west-1.wasabisys.com/bb-weights/RealESRGAN_x2.pth.lock?AWSAccessKeyId=000R8RBVN2HP618VW0U4&Expires=1710558909&Signature=Z4jXfMqp%2FOLOXtCtZwX77JqWzMA%3D', 'RealESRGAN_x2.pth.lock')
+    ('https://s3.us-west-1.wasabisys.com/bb-weights/queratograySketch_v10.safetensors?AWSAccessKeyId=HDWU8HH7D8CP0HTLJ1FE&Expires=1711945016&Signature=TrmJ07nbRAU%2B%2BnxKmlVCFlMj3uc%3D', 'queratograySketch_v10.safetensors'),
+    ('https://s3.us-west-1.wasabisys.com/bb-weights/RealESRGAN_x2.pth?AWSAccessKeyId=HDWU8HH7D8CP0HTLJ1FE&Expires=1711945054&Signature=e5oOWVvHqR9Uch%2B3JQZ11M%2BCBvY%3D', 'RealESRGAN_x2.pth'),
+    ('https://s3.us-west-1.wasabisys.com/bb-weights/RealESRGAN_x2.pth.lock?AWSAccessKeyId=HDWU8HH7D8CP0HTLJ1FE&Expires=1711945071&Signature=ELgkW9dFuRV113Pc9fo4afkRfBU%3D', 'RealESRGAN_x2.pth.lock')
 ]
 
 print("Downloading\n")
